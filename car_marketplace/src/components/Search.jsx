63edd5b976292f3e8a661ea28329c1ec94fcde78
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-17 01:25:44 +0530

    update

--- car_marketplace/src/components/Search.jsx
+++ car_marketplace/src/components/Search.jsx
@@ -6,42 +6,47 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { Separator } from "@radix-ui/react-select";
+import { CiSearch } from "react-icons/ci";
+import Data from "./Shared/Data";
 
 const Search = () => {
   return (
     <div className="p-2 md:p-4 bg-white rounded-md md:rounded-full flex flex-col md:flex-row gap-10 items-center w-[60%]">
       <Select>
         <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
-          <SelectValue placeholder="Theme" />
+          <SelectValue placeholder="Cars" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
+          <SelectItem value="light">New</SelectItem>
+          <SelectItem value="dark">Old</SelectItem>
         </SelectContent>
       </Select>
-
+      <Separator orientation="vertical" className="hidden md:block" />
       <Select>
         <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
-          <SelectValue placeholder="Theme" />
+          <SelectValue placeholder="Car makes" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
+          {Data.CarMakes.map((maker, index) => (
+            <SelectItem value={maker.name}>{maker.name}</SelectItem>
+          ))}
         </SelectContent>
       </Select>
-
+      <Separator orientation="vertical" className="hidden md:block" />
       <Select>
-        <SelectTrigger className="w-[180px] outline-none md:border-none w-full shadow-none text-lg">
-          <SelectValue placeholder="Theme" />
+        <SelectTrigger className="outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="pricing" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="light">Light</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
+          {Data.Pricing.map((price, index) => (
+            <SelectItem value={price.amount}>{price.amount}</SelectItem>
+          ))}
         </SelectContent>
       </Select>
+      <div>
+        <CiSearch className="text-[50px] bg-primary rounded-full p-3 text-white hover:scale-105 transition-all cursor-pointer" />
+      </div>
     </div>
   );
 };
