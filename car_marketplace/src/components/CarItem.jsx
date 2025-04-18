bba5ab89af0ee14e8856eae30ec14c34e0d4aa43
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-18 14:41:20 +0530

    update

--- car_marketplace/src/components/CarItem.jsx
+++ car_marketplace/src/components/CarItem.jsx
@@ -1,19 +1,46 @@
 import React from "react";
 import { Separator } from "./ui/separator";
 import { BsFillFuelPumpDieselFill } from "react-icons/bs";
+import { MdSpeed } from "react-icons/md";
+import { GiGearStickPattern } from "react-icons/gi";
+import { MdOpenInNew } from "react-icons/md";
 
 const CarItem = ({ car }) => {
   return (
-    <div>
-      <img src={car?.image} width={300} height={250} className="rounded-t-xl" />
+    <div className="rounded-xl bg-white border shadow:md cursor-pointer">
+      <h2 className="absolute m-2 bg-green-500 px-2 rounded-full text-sm text-white">
+        New
+      </h2>
+      <img
+        src={car?.image}
+        width={"100%"}
+        height={250}
+        className="rounded-t-xl"
+      />
       <div className="p-4">
         <h2 className="font-bold text-black text-lg mb-2">{car?.name}</h2>
         <Separator />
-        <div>
-          <div>
-            <BsFillFuelPumpDieselFill />
+        <div className="grid grid-cols-3 mt-5">
+          <div className="flex flex-col items-center">
+            <BsFillFuelPumpDieselFill className="text-lg mb-2" />
             <h2>{car.miles}Miles</h2>
           </div>
+          <div className="flex flex-col items-center">
+            <MdSpeed className="text-lg mb-2" />
+            <h2>{car.fuelType}</h2>
+          </div>
+          <div className="flex flex-col items-center">
+            <GiGearStickPattern className="text-lg mb-2" />
+            <h2>{car.gearType}</h2>
+          </div>
+        </div>
+        <Separator className="my-2" />
+        <div className="flex items-center justify-between">
+          <h2 className="font-bold text-xl">${car.price}</h2>
+          <h2 className="text-primary text-sm flex gap-2 items-center">
+            view details
+            <MdOpenInNew />
+          </h2>
         </div>
       </div>
     </div>
