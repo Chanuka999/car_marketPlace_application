d8da22d6134e54d05126e6f54d827c8e7a72b17f
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-18 14:42:10 +0530

    update

--- car_marketplace/src/components/Shared/FakeData.jsx
+++ car_marketplace/src/components/Shared/FakeData.jsx
@@ -9,7 +9,7 @@ function createRandomeCarList() {
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-2SJLpCcUmj9z9rG566JJaG2xGolPlWQ4OQgC5lAPe7JUUSv9sTblbro&s",
     miles: 1000,
-    gearTypes: "Automatic",
+    gearType: "Automatic",
     price: faker.finance.amount({ min: 4000, max: 20000 }),
   };
 }
