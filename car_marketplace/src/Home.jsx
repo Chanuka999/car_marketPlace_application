9c692727da8857af7572034ca1f1e227f10f4680
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-18 14:40:30 +0530

    update

--- car_marketplace/src/Home.jsx
+++ car_marketplace/src/Home.jsx
@@ -5,6 +5,8 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Category from "./components/Category";
 import MostSearchedCar from "./components/MostSearchedCar";
+import InfoSection from "./components/InfoSection";
+import Footer from "./components/Footer";
 
 const Home = () => {
   return (
@@ -16,6 +18,8 @@ const Home = () => {
       <Hero />
       <Category />
       <MostSearchedCar />
+      <InfoSection />
+      <Footer />
     </div>
   );
 };
