9dc7e1f5ef228c6844fc309d47f49549c7c172b9
Chanuka999 <chanukaranditha99@gmail.com>
2025-04-19 11:40:57 +0530

    update

--- car_marketplace/drizzle.config.js
+++ car_marketplace/drizzle.config.js
@@ -3,6 +3,6 @@ export default {
   dialect: "postgresql",
   schema: "./configs/schema.js",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_iwzvQeFEm02B@ep-tiny-bread-a4hh7922-pooler.us-east-1.aws.neon.tech/car_marketPlace?sslmode=require",
+    url: "postgresql://neondb_owner:npg_iwzvQeFEm02B@ep-tiny-bread-a4hh7922.us-east-1.aws.neon.tech/car-marketPlace?sslmode=require",
   },
 };
