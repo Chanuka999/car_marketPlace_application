a6515cc26ed3ac549f9357792407fb0a8da05e4f
Chanuka999 <chanukaranditha99@gmail.com>
2025-05-02 17:21:18 +0530

    change codes

---
+++ car_marketplace/src/Profile/components/MyListing.jsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { db } from "./../../../configs/Index";
+import { CarImages, CarListing } from "./../../../configs/schema";
+import { desc, eq } from "drizzle-orm";
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
+const MyListing = () => {
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && GetUserCarListing();
+  }, [user]);
+  const GetUserCarListing = async () => {
+    const result = await db
+      .select()
+      .from(CarListing)
+      .leftJoin(CarImages, eq(CarListing.id, CarImages.carListingId))
+      .where(eq(CarListing.createdBy, user?.primaryEmailAddress?.emailAddress))
+      .orderBy(desc(CarListing.id));
+
+    console.log(result);
+  };
+  return (
+    <div className="mt-6">
+      <div className="flex justify-between items-center">
+        <h2 className="font-bold text-4xl">My Listing</h2>
+        <Link to={"/add_listing"}>
+          <Button>+ Add New Listing</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MyListing;
